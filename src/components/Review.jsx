731461a58069c5d96d7bea176bf97675ba0ff42b
Sommiei <jideoforsommie@gmail.com>
2024-04-21 16:00:31 +0100

    updated the colour,the reset ans forgot password

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -35,7 +35,7 @@ export const Review = () => {
   return (
     <div className="bg-gray-100 py-8 px-10">
       <div className="max-w-5xl mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8 text-center text-black">
+        <h2 className="text-5xl font-bold mb-8 text-center text-black">
           Reviews
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
