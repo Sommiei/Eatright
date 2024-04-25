83c32b217156f6a13b392a07d631628c19d2b5ce
Sommiei <jideoforsommie@gmail.com>
2024-04-25 21:33:06 +0100

    updated the her section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ export const Hero = () => {
     <section className="eat-right_hero container mx-auto flex flex-col md:flex-row items-center px-10">
       {/* Text area */}
       <div className="eat-right_hero-text max-w-md md:max-w-lg md:pr-10 pt-5 md:pt-0 text-black"> {/* Adjust padding */}
-        <h1 className="text-5xl md:text-4xl font-bold leading-tight mb-4 font-poppins t">
+        <h1 className="text-5xl md:text-4xl font-bold leading-tight mb-4 font-poppins ">
           Discover a <span className="text-[#846B57]">World</span> of Fresh, Nutritious Delights!
         </h1>
         <p className="text-md md:text-xl mb-6 font-poppins text-black">
